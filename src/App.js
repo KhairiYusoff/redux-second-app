e70e18118895ba82160f9226339802980e700e73
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-12-22 23:57:35 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const showCart = useSelector(state => state.toggle.showCart);
   const cart = useSelector(state => state.cart)
 
-  useEffect(() => {
-    fetch('https://react-http-8cea9-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart)
-    })
-  }, [cart])
+
 
   return (
     <Layout>
