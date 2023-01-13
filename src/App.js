09826bdacd72d940ae5c5af933b365202e3282cc
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2023-01-14 00:05:30 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
 
 function App() {
+  const dispatch = useDispatch()
   const showCart = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart)
 
   useEffect(() => {
-    fetch('https://react-http-8cea9-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart)
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!'
+      }))
+      const response = await fetch('https://react-http-8cea9-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      })
+
+      if (!response.ok) {
+        throw new Error("FAIL")
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sending cart data success!'
+      })
+      )
+    };
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data error!'
+        })
+      )
     })
-  }, [cart])
+  }, [cart, dispatch])
 
   return (
     <Layout>
